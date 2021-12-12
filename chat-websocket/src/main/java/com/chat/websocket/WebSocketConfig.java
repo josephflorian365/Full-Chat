c5fed31023a767fcf8b166ab52c7562aa68dd311
Joseph Jesús Florián Flores <53907643+josephflorian365@users.noreply.github.com>
2021-12-11 20:48:51 -0500

    update host in WebSocketConfig.

--- chat-websocket/src/main/java/com/chat/websocket/WebSocketConfig.java
+++ chat-websocket/src/main/java/com/chat/websocket/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat-websocket") //server path in springboot
-        .setAllowedOrigins("http//localhost:4200")
+        .setAllowedOrigins("http://localhost:4200")
                 .withSockJS();
     }
 
